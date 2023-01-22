624dff5835032e73e858ab1886ea8368e1bf5b52
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2023-01-22 17:59:14 +0600

    .

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
 
-// app.get("*", (req, res) => {
-//     app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-//   });
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
 
 //Handleing Uncaught Exceptions
 process.on("uncaughtException", (err) => {
