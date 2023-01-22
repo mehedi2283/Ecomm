f16b06c9204f0be2766f68f2f65999df4ca2db72
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2023-01-22 17:56:51 +0600

    .

--- backend/app.js
+++ backend/app.js
@@ -31,10 +31,10 @@ app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
 
-// app.get("*", (req, res) => {
-//   app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 // Middleware for Error
 app.use(errorMiddleWare);
